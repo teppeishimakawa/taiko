c0fd6bcfa687caf2c4fda5e003fe192e6506b175
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-23 15:19:13 +0900

    3

--- taiko.js
+++ taiko.js
@@ -1,7 +1,7 @@
 var audio=document.getElementById("sound");
 var tapp=document.getElementById("btn");
 
-document.getElementById("btn").addEventListener('click',function()
+tapp.addEventListener('click',function()
 {
 audio.play();
 tapp.src="./btn2.png"
