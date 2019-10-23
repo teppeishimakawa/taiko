ae3d98557c9c345e65b731b79812e9c0da0b4e8f
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-23 15:36:43 +0900

    15

--- taiko.js
+++ taiko.js
@@ -5,5 +5,5 @@ tapp.addEventListener('click',function()
 {
 tapp.src="./btn2.png"
 audio.play();
-setTimeout(function(){tapp.src="./btn.png"},300)
+setTimeout(function(){tapp.src="./btn.png"},150)
 });
