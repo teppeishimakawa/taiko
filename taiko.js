7b6d64caff0decee68b9c073cf986ebd4e060d56
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-24 11:00:08 +0900

    115

--- taiko.js
+++ taiko.js
@@ -4,22 +4,19 @@ img[0].src='./btn.png';
 img[1]=new Image();
 img[1].src='./btn2.png';
 
-var audio=new Audio("./pon.wav");
+var audio=new Audio("./pon.mp3");
 
 var tapp=document.getElementById("btn");
 
 tapp.src=img[0].src;
-img[0]=new Image();
-img[0].src='./btn.png';
+
 
 tapp.onclick=function()
 {
 tapp.src=img[1].src;
-img[1]=new Image();
-img[1].src='./btn2.png';
 
 audio.play();
 //連続再生用にキャッシュ用にaudio要素を作成
-audio=new Audio("./pon.wav");
+audio=new Audio("./pon.mp3");
 setTimeout(function(){tapp.src=img[0].src},150)
 };
