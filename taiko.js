5c42fadb346c6f2165b761619b3b75ef96d75fce
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-24 18:29:47 +0900

    186

--- taiko.js
+++ taiko.js
@@ -73,7 +73,7 @@ var defdef;
                 }
 
                 console.log(def - localStorage.getItem('ls_def'));
-                console.log(Math.max(def,localStorage.getItem('ls_def'))%Math.min(def,localStorage.getItem('ls_def')));
+                console.log((Math.max(def,localStorage.getItem('ls_def')))%(Math.min(def,localStorage.getItem('ls_def'))));
 
             if(cnt > 2){
              def_total = def_total + Math.abs(parseInt(defdef));
