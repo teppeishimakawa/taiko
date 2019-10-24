63799f63cf1d22c0b84ab8acba7ff2a2d3a8468a
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-24 11:13:15 +0900

    116

--- taiko.js
+++ taiko.js
@@ -11,6 +11,24 @@ var tapp=document.getElementById("btn");
 tapp.src=img[0].src;
 
 
+
+tapp.addEventListener("touchstart",function()
+{
+tapp.src=img[1].src;
+audio.play();
+//連続再生用にキャッシュ用にaudio要素を作成
+audio=new Audio("./pon.mp3");
+});
+
+
+tapp.addEventListener("touchend",function()
+{
+tapp.src=img[0].src;
+});
+
+
+
+/*
 tapp.onclick=function()
 {
 tapp.src=img[1].src;
@@ -20,3 +38,21 @@ audio.play();
 audio=new Audio("./pon.mp3");
 setTimeout(function(){tapp.src=img[0].src},150)
 };
+*/
+
+
+
+//拡大縮小ガード
+try
+{
+document.addEventListener('touchstart', function(e)
+ {
+  if (event.touches.length > 1)
+  {
+    event.preventDefault();
+  }
+ }, {passive: false});
+}catch(e)
+{
+ false
+}
