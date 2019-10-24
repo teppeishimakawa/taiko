7a5045ba6a49da014b31bce9226736723a4122c9
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-24 09:53:40 +0900

    1005

--- taiko.js
+++ taiko.js
@@ -1,9 +1,17 @@
+var img=[];
+img[0]=new Image();
+img[0].src='./btn.png';
+img[1]=new Image();
+img[1].src='./btn2.png';
+
 var audio=document.getElementById("sound");
 var tapp=document.getElementById("btn");
 
-tapp.addEventListener('click',function()
+tapp.src=img[0].src;
+
+tapp.onclick=function()
 {
 tapp.src="./btn2.png"
 audio.play();
 setTimeout(function(){tapp.src="./btn.png"},150)
-});
+};
