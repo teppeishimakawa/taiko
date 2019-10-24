d5c9954ccce98c3351df11159321f1ae3b0abb97
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-24 13:46:42 +0900

    137

--- taiko.js
+++ taiko.js
@@ -33,7 +33,7 @@ audio.load();
 dv.addEventListener("touchstart",function(e)
 {
 e.preventDefault();
-audio.start();
+audio.play();
 //tapp.src=img[1].src;
 //連続再生用にキャッシュ用にaudio要素を作成
 audio=new Audio("./pon.mp3");
