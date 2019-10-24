5acf73b812c076bb493ba1ba1f71263a748529d5
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-24 16:49:46 +0900

    169

--- taiko.js
+++ taiko.js
@@ -13,13 +13,6 @@ var def_total=0;
 
     // コンテキストを生成
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
-
-
-        document.getElementById("btn").addEventListener(isSP ? 'touchstart' : 'click', function()
-        {
-
-
-
     ctx = new AudioContext();
 
     // 音源ファイルをバイナリデータとして取得
@@ -43,9 +36,8 @@ var def_total=0;
 
     isSP = typeof window.ontouchstart !== 'undefined';
 
-
-
-            
+        document.getElementById("btn").addEventListener(isSP ? 'touchstart' : 'click', function()
+        {
             var bufferSource;
             bufferSource = ctx.createBufferSource();
             bufferSource.buffer = data;
@@ -55,7 +47,7 @@ var def_total=0;
             try{navigator.vibrate( 500 )}catch(e){return false}
 
             cnt++;
-
+            console.log(cnt);
             if(localStorage.getItem('ls_time'))
              {
              def=day.getTime() - localStorage.getItem('ls_time');
@@ -66,7 +58,7 @@ var def_total=0;
             localStorage.setItem('ls_time', old_time);
 
             console.log(def_total);
-            console.log(cnt);
+
         });
 
     })
