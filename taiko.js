8a60d1140a47cb99ee73adc60747c7274df2e4df
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-24 18:34:12 +0900

    187

--- taiko.js
+++ taiko.js
@@ -72,8 +72,8 @@ var defdef;
                  );
                 }
 
-                console.log(def - localStorage.getItem('ls_def'));
-                console.log((Math.max(def,localStorage.getItem('ls_def')))%(Math.min(def,localStorage.getItem('ls_def'))));
+                console.log(def);
+                console.log(localStorage.getItem('ls_def'));
 
             if(cnt > 2){
              def_total = def_total + Math.abs(parseInt(defdef));
