85fa425790afb5ca7c77cd14a57b60a181b9d751
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-25 15:57:10 +0900

    267

--- taiko.js
+++ taiko.js
@@ -104,7 +104,7 @@ document.getElementById("clr").onclick=function(){localStorage.clear();}
 
 document.getElementById("kekka").onclick=function()
 {
-document.getElementById("disp").innerHTML=100 - def_total/cnt/10 - Math.abs((cnt - bpm*duration/60)/5);
+document.getElementById("disp").innerHTML=Math.floor(100 - def_total/cnt/10 - Math.abs((cnt - bpm*duration/60)/5)) + "点!";
 };
 
 document.getElementById("set").onclick=function()
